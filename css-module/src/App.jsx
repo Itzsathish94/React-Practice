4e62a79feabe56bc4da23e1efa00de9631c31fca
Sathish <rhbsk1994@gmail.com>
2025-01-12 09:32:50 +0530

    Css-module

---
+++ css-module/src/App.jsx
@@ -0,0 +1,17 @@
+import Button from './Button.jsx';
+import Button3 from './Button3.jsx';
+import Button2 from './button2.jsx/button2.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Button/>
+    <Button2/>
+    <Button3/>
+    </>
+  )
+}
+
+export default App
---
+++ css-module/src/Button3.jsx
@@ -0,0 +1,18 @@
+function Button3(){
+
+    const style = {
+        "background-color": "hsl(128, 87.00%, 45.10%)",
+    "border": "none",
+    "border-radius": "5px",
+    "color": "white",
+    "padding": "10px 20px",
+    "cursor": "pointer"
+
+    }
+
+    return(
+        <button className="button3" style={style} >Click me</button>
+    );
+}
+
+export default Button3
