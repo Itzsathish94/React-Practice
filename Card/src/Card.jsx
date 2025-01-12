9f62ae23f7aab5092e8a136d122e27c714f9a4d3
Sathish <rhbsk1994@gmail.com>
2025-01-12 08:41:36 +0530

    Card component

---
+++ Card/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return (
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+            <h2 className="card-title">Bro Camper</h2>
+            <p className="card-details">I learn coding and play games</p>
+        </div>
+    );
+}
+
+export default Card
